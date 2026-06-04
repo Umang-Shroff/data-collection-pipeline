793c65de8c9d7009a96457fc0e48f10d5ff18873
Umang <soyumang55@gmail.com>
2026-06-04 16:09:30 +0530

    fix: EventStore made threadSafe and concurrency safe

--- src/events/EventStore.java
+++ src/events/EventStore.java
@@ -1,24 +1,28 @@
 package events;
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.List;
 
 public class EventStore implements EventRepository {
     private List<Event> events;
 
     public EventStore () {
-        events = new ArrayList<>();
+        this.events = new CopyOnWriteArrayList<>();
     }
 
+    @Override
     public void save(Event event) {
         events.add(event);
     }
 
+    @Override
     public void saveBatch(List<Event> batch) {
         events.addAll(batch);
     }
 
+    @Override
     public List<Event> getAllEvents(){
-        return new ArrayList<>(events);
+        return new CopyOnWriteArrayList<>(events);
     }
 }
